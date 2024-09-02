baae1b05437c10bcd4bc93e1a3db3728f0a2ae18
gabriel saito <gabrielpereira050201@hotmail.com>
2024-09-02 12:41:15 -0300

    Added: base project

---
+++ src/app.tsx
@@ -0,0 +1,91 @@
+import { clsx } from 'clsx'
+import { useEffect, useRef, useState } from 'react'
+import data from './data.json'
+
+export function App() {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [filter, setFilter] = useState('')
+
+  function getFilteredData() {
+    if (!filter) {
+      return data
+    }
+
+    return data.filter(
+      (pokemon) =>
+        pokemon.related.filter((r) =>
+          r.toLowerCase().includes(filter.toLowerCase()),
+        ).length > 0,
+    )
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.ctrlKey && event.key === 'f') {
+        if (!inputRef.current) return
+
+        event.preventDefault()
+        inputRef.current.focus()
+        inputRef.current.value = ''
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
+  return (
+    <div className='flex h-svh flex-col p-4'>
+      <div className='h-full overflow-auto'>
+        <table className='w-full'>
+          <thead>
+            <tr>
+              <th align='left'>Name</th>
+              <th align='left'>Max CP</th>
+              <th align='left'>GL</th>
+              <th align='left'>UL</th>
+              <th align='left'>ML</th>
+              <th align='left'>Tier</th>
+              <th align='left'>Missing</th>
+              <th align='left'>IV</th>
+            </tr>
+          </thead>
+
+          <tbody className='h-full'>
+            {getFilteredData().map((pokemon, i) => (
+              <tr
+                key={pokemon.name}
+                className={clsx(
+                  'h-16 px-2 hover:bg-neutral-200',
+                  i % 2 === 0 && 'bg-neutral-100',
+                )}
+              >
+                <td align='left'>{pokemon.name}</td>
+                <td align='left'>{pokemon.maxCP}</td>
+                <td align='left'>{pokemon.gl || '-'}</td>
+                <td align='left'>{pokemon.ul || '-'}</td>
+                <td align='left'>{pokemon.ml || '-'}</td>
+                <td align='left'>{pokemon.tier || '-'}</td>
+                <td align='left'>{pokemon.missing?.toString() || '-'}</td>
+                <td align='left'>{pokemon.iv || '-'}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div>
+        <input
+          ref={inputRef}
+          type='text'
+          className='w-full rounded-full border px-4 py-2'
+          placeholder={`Search a pokemon (${data.length} entries)`}
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </div>
+    </div>
+  )
+}
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: { passes: 2 },
+      mangle: true,
+      format: { comments: false },
+    },
+  },
+})
